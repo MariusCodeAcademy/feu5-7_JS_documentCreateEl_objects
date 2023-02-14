23d49038e85fe7c86d3503c3b39fb3029bd3a850
Marius CA <marius@example.com>
2023-02-14 11:27:52 +0200

    create el

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 'use strict';
 console.log('main.js file was loaded');
+
+// document.createElement(tagName)
+
+// 1. sukuriam h2 el
+//
+const h2El = document.createElement('h2'); // <h2></h2>
+// 2 pridedam text
+h2El.textContent = 'I am dynamic H2 el'; // <h2>I am dynamic H2 el</h2>
+console.log(h2El);
+
+// 3 patalpinti elementa i dom append
+// parent.append(newEl)
+document.body.append(h2El);
+
+document.getElementById('app').append(h2El);
